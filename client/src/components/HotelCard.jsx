6b17d0d82d3cd3952216ffebff09b824303863c7
KRAZYFLASH <muhammadfakhri.hsb@gmail.com>
2025-06-07 15:09:51 +0700

    pembuatan navbar dan setup clarke serta prebuiltUI untuk UI nya

---
+++ client/src/components/HotelCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets'
+
+const HotelCard = ({room, index}) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default HotelCard
