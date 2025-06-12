7ba0d38d68f17ead3fad74cd52c09efe6381016f
KRAZYFLASH <muhammadfakhri.hsb@gmail.com>
2025-06-12 09:55:43 +0700

    Pembuatan Room Details

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -117,7 +117,7 @@ export const userDummyData = {
     "_id": "user_2unqyL4diJFP1E3pIBnasc7w8hP",
     "username": "Great Stack",
     "email": "user.greatstack@gmail.com",
-    "image": "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
+    "image": "https://th.bing.com/th/id/OSK.XwF9z-jkcHKVl6_q7Ok9uIrP--syGZFZL2QX1tKf0Gs?w=224&h=200&c=12&rs=1&o=6&dpr=1.5&pid=SANGAM",
     "role": "hotelOwner",
     "createdAt": "2025-03-25T09:29:16.367Z",
     "updatedAt": "2025-04-10T06:34:48.719Z",
